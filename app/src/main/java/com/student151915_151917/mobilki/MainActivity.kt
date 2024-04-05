90f5faae5009d0072b578b468b13616cdad59177
nikiaker <md.macek@gmail.com>
2024-04-05 13:42:15 +0200

    Dodano binding

--- app/src/main/java/com/student151915_151917/mobilki/MainActivity.kt
+++ app/src/main/java/com/student151915_151917/mobilki/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.student151915_151917.mobilki
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
